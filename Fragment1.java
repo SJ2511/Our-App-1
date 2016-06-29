1d0f969bcaa3ade0bb591743a71f5aeb3b753cdb
SJ2511 <smartsj2511@gmail.com>
2016-06-29 15:29:43 +0530

    Add files via upload

--- Fragment1.java
+++ Fragment1.java
@@ -10,18 +10,73 @@ package com.example.dell.intents;
         import android.os.Bundle;
 
         import android.support.v4.app.Fragment;
+        import android.support.v7.widget.DefaultItemAnimator;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
 
+        import java.util.ArrayList;
+        import java.util.List;
+
 
 public class Fragment1 extends Fragment {
+    private RecyclerView recyclerView;
+    private List<Event> eventList = new ArrayList<>();
+    private EventsAdapter mAdapter;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1, container, false);
+        View rootView = inflater.inflate(R.layout.fragment1, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+
+        mAdapter = new EventsAdapter(getActivity(),eventList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),LinearLayoutManager.VERTICAL));
 
 
+        recyclerView.setAdapter(mAdapter);
+
+        prepareEventData();
+        return rootView;
     }
 
+
+
+    private void prepareEventData() {
+        Event event = new Event("Mad Max: Fury Road", "Action & Adventure", "2015", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "w5", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "2r5", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "e15", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "e15", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "2w", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "q5", "yyyy");
+        eventList.add(event);
+
+        event = new Event("Mad Max: Fury Road", "Action & Adventure", "v5", "yyyy");
+        eventList.add(event);
+
+
+        mAdapter.notifyDataSetChanged();
+    }
 }
+
+
+
 
